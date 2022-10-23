b22347294bdb36a74d5dac039724d2d9679e4bf8
Guilherme Souza <gss214@hotmail.com>
2022-10-22 21:09:33 -0300

    feat: create routers

---
+++ app/routers/pokemons.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from .. import schemas, database
+from ..repository import pokemon as pokemon_repository
+from ..repository import stats as stats_repository
+from sqlalchemy.orm import Session
+
+router = APIRouter(
+    prefix='/pokemon',
+    tags=['Pokemons']
+)
+
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Pokemon)
+def create_pokemon(pokemon: schemas.Pokemon, db: Session = Depends(database.get_db)):
+    db_pokemon = pokemon_repository.create(db, pokemon)
+    stats_data = pokemon.dict().pop('stats', None)
+    stats_data['pokemon_id'] = db_pokemon.id
+    stats_repository.create(db, stats_data)
+    return pokemon
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_pokemon(id:int, db: Session = Depends(database.get_db)):
+    if not pokemon_repository.delete_by_id(db,id):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'pokemon with the id {id} is not found')
+    if not stats_repository.delete_by_pokemon_id(db, id):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'pokemon_stats with the pokemon_id {id} is not found')
+    return {'ok':True}
+
+@router.get('/{id}', status_code=status.HTTP_200_OK)
+def get_pokemon(id:int, db: Session = Depends(database.get_db)):
+    pokemon = pokemon_repository.get_by_id(db, id)
+    if not pokemon.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'pokemon with the id {id} is not found')
+    pokemon = pokemon.first()
+    pokemon_stats = stats_repository.get_by_pokemon_id(db, pokemon.id)
+    if not pokemon_stats.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'pokemon_stats with the id {id} is not found')
+    pokemon.stats = [pokemon_stats.first()]
+    return pokemon
---
+++ app/routers/users.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from .. import schemas, database
+from sqlalchemy.orm import Session
+from ..repository import users as users_repository
+
+router = APIRouter(
+    prefix='/user',
+    tags=['Users']
+)
+
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
+def create_user(request:schemas.User, db: Session = Depends(database.get_db)):
+    return users_repository.create(db, request)
+
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
+def get_user(id:int, db: Session = Depends(database.get_db)):
+    user =  users_repository.get_by_id(db,id)
+    if not user.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'user with the id {id} is not found')
+    return user.first()
